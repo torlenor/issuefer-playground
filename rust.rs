b8b45dc6c500ed7e65c6a9a1e951d52f7c99fcad
Hans-Peter Schadler <hps@abyle.org>
2020-05-10 15:16:38 +0200

    update

--- rust.rs
+++ rust.rs
@@ -1,2 +1,6 @@
 // TODO: This is a todo which shall be converted to a GitHub issue
-// TODO (#12): This is a todo already has an issue number
+    
+fn main() -> Result<(), Box<dyn Error>> {
+    // TODO: This is another issue which shall be convreted to a GitHub issue.
+    Ok(())
+}
