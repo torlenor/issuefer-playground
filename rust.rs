cc91ed9c31c54cb36551672571eda7048dd3292d
Hans-Peter Schadler <hps@abyle.org>
2020-05-19 09:13:27 +0200

    Add TODO #32

--- rust.rs
+++ rust.rs
@@ -1,7 +1,7 @@
 // TODO (#25): This is a todo which shall be converted to a GitHub issue
     
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO: Some new todo
+    // TODO (#32): Some new todo
     Ok(())
     // TODO: another todo
 }
