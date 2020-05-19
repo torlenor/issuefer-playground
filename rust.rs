b750b7b7f6ca1460498551bbb504b6c46e8f998f
Hans-Peter Schadler <hps@abyle.org>
2020-05-19 09:20:07 +0200

    Remove TODO #32

--- rust.rs
+++ rust.rs
@@ -1,6 +1,5 @@
     
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO (#32): Some new todo
     Ok(())
-    // TODO (#33): another todo
+                        // TODO (#33): another todo  
 }
